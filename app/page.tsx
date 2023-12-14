d80c1086d118cd76e7b395e5c26a9c58b2d74821
Jay Keraliya <jaykeraliya0@gmail.com>
2023-12-14 21:58:52 +0530

    Add authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <main className="">Home Page</main>;
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session = await getServerSession();
+  console.log("session: ", session);
+
+  return (
+    <main className="">
+      {session ? (
+        <div>
+          name: {session.user?.name}
+          <br />
+          email: {session.user?.email}
+        </div>
+      ) : (
+        <div>Not signed in</div>
+      )}
+    </main>
+  );
 }
