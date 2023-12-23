b4e352436078840a7aaaacf1360343f7f3be4779
Jay Keraliya <jaykeraliya0@gmail.com>
2023-12-23 17:49:40 +0530

    Update layout.tsx and utils.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,16 @@
-import type { Metadata } from "next";
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toaster";
+import Provider from "@/context/Provider";
+import { cn, constructMetadata } from "@/lib/utils";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Provider from "@/context/Provider";
-import { cn } from "@/lib/utils";
-import { Toaster } from "@/components/ui/toaster";
-import Navbar from "@/components/Navbar";
 
 import "react-loading-skeleton/dist/skeleton.css";
 import "simplebar-react/dist/simplebar.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "DocQuery",
-  description: "Ask questions to PDF documents",
-};
+export const metadata = constructMetadata();
 
 export default function RootLayout({
   children,
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -10,4 +11,48 @@ export function absoluteUrl(path: string) {
   if (process.env.VERCEL_URL)
     return `https://doc-query.jaykeraliya.tech${path}`;
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+}
+
+export function constructMetadata({
+  title = "Doc Query",
+  description = "Doc Query is a documentation search engine for everyone.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@jaykeraliya0",
+    },
+    icons,
+    metadataBase: new URL("https://doc-query.jaykeraliya.tech/"),
+    themeColor: "#ffffff",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
