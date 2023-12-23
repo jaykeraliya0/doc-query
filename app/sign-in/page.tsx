fbf268255397286f30fbac84ca9689a36ef158c6
Jay Keraliya <jaykeraliya0@gmail.com>
2023-12-23 14:44:17 +0530

    Remove unused import and update middleware configuration

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Github } from "lucide-react";
 import { signIn } from "next-auth/react";
 
 export default function Page() {
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,27 @@
-// export const config = {
-//   matcher: ["/dashboard/:path*"],
-// };
+import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
 
-// export default midd;
+export default async function middleware(
+  req: NextRequest,
+  event: NextFetchEvent
+) {
+  const session = await getServerSession();
 
-export { default } from "next-auth/middleware";
+  if (req.nextUrl.pathname.startsWith("/sign-in") && session?.user) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 
-export const config = { matcher: ["/dashboard/:path*"] };
+  const authMiddleware = withAuth({
+    pages: {
+      signIn: `/authentication/sign-in`,
+    },
+  });
+
+  return authMiddleware(req as NextRequestWithAuth, event);
+}
+
+export const config = {
+  matcher: ["/sign-in", "/dashboard/:path*"],
+};
