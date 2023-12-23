61d9d4cf78204a8576c7a3ae8ff434f9f359bee0
Jay Keraliya <jaykeraliya0@gmail.com>
2023-12-23 14:48:40 +0530

    Refactor middleware.ts to export default from next-auth/middleware

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,3 @@
-import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
-import { getToken } from "next-auth/jwt";
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
+export { default } from "next-auth/middleware";
 
-export default async function middleware(
-  req: NextRequest,
-  event: NextFetchEvent
-) {
-  const session = await getServerSession();
-
-  if (req.nextUrl.pathname.startsWith("/sign-in") && session?.user) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
-  const authMiddleware = withAuth({
-    pages: {
-      signIn: `/authentication/sign-in`,
-    },
-  });
-
-  return authMiddleware(req as NextRequestWithAuth, event);
-}
-
-export const config = {
-  matcher: ["/sign-in", "/dashboard/:path*"],
-};
+export const config = { matcher: ["/dashboard/:path*"] };
